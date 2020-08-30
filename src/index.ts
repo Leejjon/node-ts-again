72e9503504cfbd5056203881044eed66b57da67a
Leejjon <leejjon@gmail.com>
2020-08-30 21:05:00 +0200

    Forgot to add the webpack hot module replacement code.

--- src/index.ts
+++ src/index.ts
@@ -10,3 +10,29 @@ app.get( "/", ( req, res ) => {
 const server = app.listen(PORT, () => {
     console.log(`Listening on port ${PORT}`);
 });
+
+/**
+ * Webpack HMR Activation
+ */
+
+type ModuleId = string | number;
+
+interface WebpackHotModule {
+    hot?: {
+        data: any;
+        accept(
+            dependencies: string[],
+            callback?: (updatedDependencies: ModuleId[]) => void,
+        ): void;
+        accept(dependency: string, callback?: () => void): void;
+        accept(errHandler?: (err: Error) => void): void;
+        dispose(callback: (data: any) => void): void;
+    };
+}
+
+declare const module: WebpackHotModule;
+
+if (module.hot) {
+    module.hot.accept();
+    module.hot.dispose(() => server.close());
+}
