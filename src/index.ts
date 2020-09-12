83971b7dc4a994b3c32cf603abb90332296bb22f
Leejjon <leejjon@gmail.com>
2020-09-12 21:42:19 +0200

    An error handler.

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,28 @@
-import express from "express";
+import express, {Request, Response, NextFunction} from "express";
 import {postComment} from "./controller/CommentsController";
 
 const PORT = process.env.PORT || 8080;
 
 const app = express();
 
-app.use(express.text({ type: 'application/json' }));
+app.use(express.json());
+
+interface SyntaxErrorWithStatusAndBody extends SyntaxError {
+    status: number;
+    body: string;
+}
+
+app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+    // This check makes sure this is a JSON parsing issue, but it might be
+    // coming from any middleware, not just body-parser:
+    if ((err as SyntaxErrorWithStatusAndBody).status === 400 && 'body' in err) {
+        const bodyWithInvalidJson = (err as SyntaxErrorWithStatusAndBody).body;
+        console.error(`Somebody tried to do a request with invalid json: ${bodyWithInvalidJson}`);
+        return res.sendStatus(400); // Bad request
+    }
+    // If it's another error, let the default handler handle it.
+    next();
+});
 
 app.post("/comments", postComment);
 
