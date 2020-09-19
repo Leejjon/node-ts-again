12c34728ddd9581bf0c760f53b3e4ddde4e13aa4
Leejjon <leejjon@gmail.com>
2020-09-19 19:15:04 +0200

    Manual validation on undefined.

--- src/controller/CommentsController.ts
+++ src/controller/CommentsController.ts
@@ -4,10 +4,16 @@ import {CommentConfirmation, NewCommentRequest} from "../model/Comment";
 export const postComment = async (req: Request, res: Response) => {
     try {
         const newCommentRequest: NewCommentRequest = req.body;
-        console.log(`Stored comment from ${newCommentRequest.nickname}.`);
-        let response: CommentConfirmation = {id: 'commentId', timestamp: new Date()};
-        res.status(200);
-        res.send(response);
+
+        if (newCommentRequest.message && newCommentRequest.nickname) {
+            console.log(`Stored comment from ${newCommentRequest.nickname}.`);
+            let response: CommentConfirmation = {id: 'commentId', timestamp: new Date()};
+            res.status(200);
+            res.send(response);
+        } else {
+            res.status(400);
+            res.send("Invalid request.");
+        }
     } catch (error) {
         res.status(400);
         res.send("Invalid request.");
