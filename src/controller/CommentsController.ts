be4ce0b2f5abbcfa9d6189039bf1f81632a66a90
Leejjon <leejjon@gmail.com>
2020-11-09 19:41:58 +0100

    Filtering out curly braces does the trick. Ugly but it works.

--- src/controller/CommentsController.ts
+++ src/controller/CommentsController.ts
@@ -4,21 +4,26 @@ import {validate, ValidationError} from "class-validator";
 
 export const postComment = async (req: Request, res: Response) => {
     try {
-        let newCommentRequest: NewCommentRequest = req.body;
-        let validationErrors: Array<ValidationError> = await validate(newCommentRequest);
 
-        if (validationErrors.length > 0) {
-            validationErrors.forEach((validationError) => {
-                console.log(validationError);
-            });
-            res.status(400);
-            res.send("Invalid request.");
-        } else {
+        if (!(req.body.startsWith('{') && req.body.endsWith('}'))) {
+            console.log('bad');
+        }
+
+        let newCommentRequest: NewCommentRequest = JSON.parse(req.body);
+        // let validationErrors: Array<ValidationError> = await validate(newCommentRequest);
+
+        // if (validationErrors.length > 0) {
+        //     validationErrors.forEach((validationError) => {
+        //         console.log(validationError);
+        //     });
+        //     res.status(400);
+        //     res.send("Invalid request.");
+        // } else {
             console.log(`Stored comment from ${JSON.stringify(newCommentRequest)}.`);
             let response: CommentConfirmation = {id: 'commentId', timestamp: new Date()};
             res.status(200);
             res.send(response);
-        }
+        // }
     } catch (error) {
         console.log(error);
         res.status(400);
